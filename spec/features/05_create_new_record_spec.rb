10a4b7a8b4f8512ac63ae653e067f0b297e05be8
Will Campbell <wibb1@comcast.net>
2020-08-25 11:15:00 -0400

    WIP-capybara testing

--- spec/features/05_create_new_record_spec.rb
+++ spec/features/05_create_new_record_spec.rb
@@ -1,18 +1,30 @@
 require 'rails_helper'
 
 feature 'user does not sign in and attempts to create a new record', %Q{ A user that is not signed in cannot create a new record } do
-  scenario 'create an invalid entry' do
-  user = FactoryBot.create(:user)
-
+  scenario 'create an invalid entry'do
+  
   visit new_record_path
+  
   click_button 'Submit'
-  expect(page).to have_content('User must exist')
+  
+  expect(page).to have_content('New Record')
   end
 end
 
-feature 'user that is signed in must complete all the required form fields before being allowed to submit the new record' do
+feature 'user that is signed in must complete all the required form fields before being allowed to submit the new record', %Q{ A user must complete the required forms to create a new record } do
+
   scenario 'create an invalid entry' do
+    user = FactoryBot.create(:user)
+
+    visit new_user_session_path
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_button 'Log in'
+
     visit new_record_path
+
     click_button 'Submit'
 
     expect(page).to have_content('New Record')
@@ -22,29 +34,33 @@ end
 feature 'user creates a new record', %Q{ As a signed in user I want to be able to create a new record } do
     scenario 'create a valid entry' do
 
+    user = FactoryBot.create(:user)
+
+    visit new_user_session_path
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_button 'Log in'
+
     visit new_record_path
     
-    page.fill_in 'Name', with: "name"
-    page.fill_in 'Description', with: "body"
-    page.fill_in 'Latitude', with: 41.8
-    page.fill_in 'Longitude', with: -70.8
-    page.select 'Good', from: 'record_success'
-    page.select "2020", from: 'record_datetime_1i'
-    page.select "August", from: 'record_datetime_2i'
-    page.select "20", from: 'record_datetime_3i'
-    page.select "11", from: 'record_datetime_4i'
-    page.select "22", from: 'record_datetime_5i'
+    fill_in 'Name', with: 'name'
+    fill_in 'Description', with: 'body'
+    fill_in 'Latitude', with: 41.8
+    fill_in 'Longitude', with: -70.8
+    select 'Good', from: 'record_success'
+    select "2020", from: 'record_datetime_1i'
+    select "August", from: 'record_datetime_2i'
+    select "16", from: 'record_datetime_3i'
+    select "11", from: 'record_datetime_4i'
+    select "22", from: 'record_datetime_5i'
+
     save_and_open_page
-    binding.pry
+
     click_button 'Submit'
 
-    expect(page).to have_content('Air Temperature')
-    expect(page).to have_content(record.name)
-    expect(page).to have_content(record.body)
-    expect(page).to have_content(record.latitude)
-    expect(page).to have_content(record.longitude)
-    expect(page).to have_content(record.date)
-    expect(page).to have_content(record.body)
+    expect(page).to have_content('Record successfully saved')
 
   end
 end
