6bbb0b13d046708b216da50bf6dfc8b5d3e29745
Will Campbell <wibb1@comcast.net>
2020-08-31 16:11:11 -0400

    add admin column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_23_135030) do
+ActiveRecord::Schema.define(version: 2020_08_31_185709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 2020_08_23_135030) do
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "user_name", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
