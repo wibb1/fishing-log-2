f3b1b8922f1f310274245adedb93deb84b7703b6
Will Campbell <wibb1@comcast.net>
2021-05-30 16:07:05 -0400

    modified species model for new configuration

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,18 +1,8 @@
 class Species < ApplicationRecord
   validates_presence_of :common_name,
                         :scientific_name,
-                        :water_column,
-                        :legal_size,
-                        :legal_size_slot,
-                        :legal_size_super,
-                        :winter_season_start,
-                        :winter_bag_limit,
-                        :spring_season_start,
-                        :spring_bag_limit,
-                        :summer_season_start,
-                        :summer_bag_limit,
-                        :fall_season_start,
-                        :fall_bag_limit
+                        :shallow_depth,
+                        :deep_depth
 
   has_and_belongs_to_many :records
 end
