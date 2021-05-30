3ce9511f98102e9e1bae23853db524b0f69a76ee
Will Campbell <wibb1@comcast.net>
2021-05-30 12:35:15 -0400

    simplified validates using validates_presence_of

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,19 +1,18 @@
 class Species < ApplicationRecord
-  validates :common_name, presence: true
-  validates :scientific_name, presence: true
-  validates :water_column, presence: true
-  validates :legal_size, presence: true
-  validates :legal_size_slot, presence: true
-  validates :legal_size_super, presence: true
-  validates :winter_season_start, presence: true
-  validates :winter_bag_limit, presence: true
-  validates :spring_season_start, presence: true
-  validates :spring_bag_limit, presence: true
-  validates :summer_season_start, presence: true
-  validates :summer_bag_limit, presence: true
-  validates :fall_season_start, presence: true
-  validates :fall_bag_limit, presence: true
+  validates_presence_of :common_name,
+                        :scientific_name,
+                        :water_column,
+                        :legal_size,
+                        :legal_size_slot,
+                        :legal_size_super,
+                        :winter_season_start,
+                        :winter_bag_limit,
+                        :spring_season_start,
+                        :spring_bag_limit,
+                        :summer_season_start,
+                        :summer_bag_limit,
+                        :fall_season_start,
+                        :fall_bag_limit
 
   has_and_belongs_to_many :records
 end
-
